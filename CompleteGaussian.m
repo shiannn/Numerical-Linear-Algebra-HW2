136a0e6a26447e78a341dedfe92378e4df465b99
shiannn <b05502087@ntu.edu.tw>
2019-11-01 11:10:12 +0800

    Commit Complete pivot for Gaussian

--- CompleteGaussian.m
+++ CompleteGaussian.m
@@ -2,26 +2,25 @@ function x = Gaussian(A,b);
     [row,col]=size(A);
     n = row;
     x = zeros(n,1);
-    pointer = [1:n];
+    Colname = [1:col];
     for k=1:n-1  
         %select row 
-        maxRowCol = -1;
+        maxVal = -1;
         for i = k:n
-            for j = k:n
-                if (A(i,j)>= maxRowCol)
-                    maxRowCol = A(i,k);
+            for j = k:n 
+                if (A(i,j)> maxVal)
+                    maxRow = A(i,j);
                     maxRowIdx = i;
                     maxColIdx = j;
                 end
             end
         end
         %row change
+        [maxRowIdx maxColIdx]
         A( [k, maxRowIdx], : ) = A( [maxRowIdx, k], : );
         b([k,maxRowIdx]) = b([maxRowIdx,k]);
-        %column change
-        A( : ,[k, maxColIdx])  = A( : ,[maxColIdx , k]);
-        %x([k, maxColIdx]) = x([maxColIdx , k]);
-        pointer([k, maxColIdx]) = pointer([maxColIdx , k]);
+        A( :,[k, maxColIdx] ) = A( : ,[k, maxColIdx]);
+        Colname([k, maxColIdx]) = Colname([maxColIdx, k]);
 
         for i=k+1:n
             xMultiplier = A(i,k)/A(k,k);
@@ -30,9 +29,8 @@ function x = Gaussian(A,b);
             end
             b(i) = b(i)-xMultiplier*b(k);
         end
+        Colname
     end
-    A
-    b
     % backsubstitution:
     x(n) = b(n)/A(n,n);
     for i=n-1:-1:1
@@ -42,5 +40,4 @@ function x = Gaussian(A,b);
         end
         x(i) = summation/A(i,i);
     end
-    pointer
 end
